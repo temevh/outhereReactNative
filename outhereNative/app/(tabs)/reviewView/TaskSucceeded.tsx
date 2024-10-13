ab2a74ae82d038258d91526d5e8c86785608d7d4
temevh <temehama@gmail.com>
2024-10-13 12:02:04 +0300

    Added more sliders and css

--- outhereNative/app/(tabs)/reviewView/TaskSucceeded.tsx
+++ outhereNative/app/(tabs)/reviewView/TaskSucceeded.tsx
@@ -1,7 +1,9 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import SliderComponent from "./SliderComponent";
 
 const TaskSucceeded = () => {
+  const handlePress = () => {};
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Good job!</Text>
@@ -12,6 +14,9 @@ const TaskSucceeded = () => {
           header={"Do you feel good after completing the task?"}
         />
       </View>
+      <TouchableOpacity style={styles.buttonStyle}>
+        <Text style={styles.textStyle}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -36,5 +41,17 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "80%",
     alignItems: "center",
+  },
+  buttonStyle: {
+    height: 100,
+    width: 260,
+    borderRadius: 14,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#568f56",
+  },
+  textStyle: {
+    color: "white",
+    fontSize: 50,
   },
 });
